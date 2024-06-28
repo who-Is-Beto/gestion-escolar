ff7b5c26c552717338b8e89efb282c7840dc6c83
WHOISBEETO <whoisbeto@gmail.com>
2024-06-27 22:48:03 -0600

    [add] project finished

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import express from "express";
 import cors from "cors";
 
 import config from "./config.js";
+import alumnoRouter from "./api/routes/alumno.route.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//routes
+app.use("/api", alumnoRouter);
 
 app.listen(config.port, () =>
   console.log(`Server is live @ ${config.hostUrl}`)
